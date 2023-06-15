7c7c2781c74c57b13321d25ea6ebc3e7b12f7021
Mustafa Çoban <mstf.cobann1@gmail.com>
2023-06-15 18:38:31 +0300

    Fetching, Details Template, Making a new form

--- cooking-coban/src/pages/recipe/Recipe.js
+++ cooking-coban/src/pages/recipe/Recipe.js
@@ -1,10 +1,40 @@
+import React, { useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
+ 
 // styles
 import './Recipe.css'
-
+ 
 export default function Recipe() {
+  const { id } = useParams()
+  const url = `http://localhost:3000/recipes/${id}`
+  const { data: recipe, isPending, error } = useFetch(url)
+ 
+  const redirectToHomepage = useHistory()
+ 
+  useEffect(() => {
+    if(error) {
+      setTimeout(() => redirectToHomepage.push('/'), 2500)
+    }
+  }, [error, redirectToHomepage]);
+  
+ 
   return (
-    <div>
-        Recipe
+    <div className="recipe">
+      {isPending && <div className="loading">Loading...</div>}
+      {error && <div className="error">{error}</div>}
+      {recipe && (
+        <div>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map(ingredient => (
+              <li>{ingredient}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
